6a2481a40d16bd350226ffc60537be73a64e0fc5
nicatsemedov777 <smdovn42@gmail.com>
2023-11-02 20:32:45 +0400

    Add entity classes' Controller, Service and Repository

---
+++ src/main/java/az/ingress/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package az.ingress.bookstore.repository;
+
+import az.ingress.bookstore.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author,String> {
+
+}
---
+++ src/main/java/az/ingress/bookstore/service/impl/AccountServiceImpl.java
@@ -0,0 +1,10 @@
+package az.ingress.bookstore.service.impl;
+
+import az.ingress.bookstore.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+}
